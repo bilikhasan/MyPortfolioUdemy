00c4181d023d1dcb2eb53e920e39d331a8647a56
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-12 00:31:49 +0300

    >>>

--- MyPortfolioUdemy/Controllers/StatisticController.cs
+++ MyPortfolioUdemy/Controllers/StatisticController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
 
 namespace MyPortfolioUdemy.Controllers
 {
-    public class StatisticController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class StatisticController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			ViewBag.v1 = context.Skills.Count();                                    //Yetenek Sayısı
+			ViewBag.v2 = context.Messages.Count();                                  //Toplam Mesaj Sayısı
+			ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();           //Okunmayan Mesaj
+			ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();            //Okunan Mesaj
+
+			ViewBag.v5 = context.Experiences.Count();
+			ViewBag.v6 = context.socialMedias.Count();
+			return View();
+		}
+	}
 }
