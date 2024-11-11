e55d11e66c0dd6dce8a1107bad04b613e1316eee
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-11 12:40:31 +0300

    ...

---
+++ MyPortfolioUdemy/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {                                                                                                   //CODEFIRST YAKLASIMI
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)                   //CODEFIRST YAKLASIMI
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; initial Catalog=MyPortfolioDb; integrated Security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> socialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
+    }
+}
