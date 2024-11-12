e7b2c5745be72774475f9cb3738707858b962bfa
bilikhasan <fatherlyy@hotmail.com.tr>
2024-11-12 16:28:59 +0300

    QAZ

--- MyPortfolioUdemy/Controllers/MessageController.cs
+++ MyPortfolioUdemy/Controllers/MessageController.cs
@@ -11,20 +11,32 @@ namespace MyPortfolioUdemy.Controllers
 			var values = context.Messages.ToList();
 			return View(values);
 		}
-		public IActionResult ChangeIsReadToTrue(int id)
+		public IActionResult ChangeIsReadToTrue(int id)		//SAYFA OLUŞTURULMAZ
 		{
 			var value = context.Messages.Find(id);
 			value.IsRead = true;
 			context.SaveChanges();
-			return RedirectToAction("Inbox");
+			return RedirectToAction("Inbox");							
 		}
-
-        public IActionResult ChangeIsReadToFalse(int id)
+        public IActionResult ChangeIsReadToFalse(int id)		//SAYFA OLUŞTURULMAZ
         {
             var value = context.Messages.Find(id);
             value.IsRead = false;
             context.SaveChanges();
             return RedirectToAction("Inbox");
         }
+		public IActionResult DeleteMessage (int id)             //SAYFA OLUŞTURULMAZ
+        {
+			var value = context.Messages.Find(id);
+			context.Messages.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult MessageDetail (int id)			//Sayfa oluştur ona göre
+		{
+			var value = context.Messages.Find(id);
+			return View(value);
+		}
     }
 }
